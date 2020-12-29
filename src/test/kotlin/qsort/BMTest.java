2b74af1b8be3d868fc90512c2a0eab92e041e0ba
Alexey <Bedrin18@mail.ru>
2020-12-30 00:48:14 +0300

    bmfixes

--- src/test/kotlin/qsort/BMTest.java
+++ src/test/kotlin/qsort/BMTest.java
@@ -14,57 +14,55 @@ import java.util.concurrent.Executors;
 
 @BenchmarkMode(Mode.All)
 @Warmup(iterations = 5)
-@Measurement(iterations = 5, batchSize = 1)
+@Measurement(iterations = 1, batchSize = 1)
 public class BMTest {
+    public static Integer itemsAmount = 100000;
     public static Qsort inst = new Qsort();
+
+    public static List<Integer> generateData(Integer size) {
+        List<Integer> a = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            a.add(Random.Default.nextInt(-1000, 1000));
+        }
+        return a;
+    }
 
     @State(Scope.Benchmark)
     public static class BenchMarkState {
-        public Integer itemsAmount = 100000;
-        public List<Integer> data1 = generateData(itemsAmount);
-        public List<Integer> data2 = generateData(itemsAmount);
-        public List<Integer> data4 = generateData(itemsAmount);
-        public List<Integer> data6 = generateData(itemsAmount);
-        public List<Integer> data8 = generateData(itemsAmount);
+        public List<Integer> dataset = generateData(itemsAmount);
 
-        public List<Integer> generateData(Integer size) {
-            List<Integer> a = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                a.add(Random.Default.nextInt(-1000, 1000));
-            }
-            return a;
-        }
+
     }
 
     @Benchmark
     public void oneThreadQSort(Blackhole blackhole, BenchMarkState state) {
-        Boolean res = inst.stSorting(state.data1);
+        Boolean res = inst.stSorting(state.dataset);
         blackhole.consume(res);
     }
 
     @Benchmark
     public void twoThreadQSort(Blackhole blackhole, BenchMarkState state) {
-        Boolean res = inst.mtSorting(state.data2, Executors.newFixedThreadPool(2));
+        Boolean res = inst.mtSorting(state.dataset, Executors.newFixedThreadPool(2));
         blackhole.consume(res);
     }
 
     @Benchmark
     public void fourThreadQSort(Blackhole blackhole, BenchMarkState state) {
-        Boolean res = inst.mtSorting(state.data4, Executors.newFixedThreadPool(4));
+        Boolean res = inst.mtSorting(state.dataset, Executors.newFixedThreadPool(4));
         blackhole.consume(res);
 
     }
 
     @Benchmark
     public void sixThreadQSort(Blackhole blackhole, BenchMarkState state) {
-        Boolean res = inst.mtSorting(state.data6, Executors.newFixedThreadPool(6));
+        Boolean res = inst.mtSorting(state.dataset, Executors.newFixedThreadPool(6));
         blackhole.consume(res);
 
     }
 
     @Benchmark
     public void eightThreadQSort(Blackhole blackhole, BenchMarkState state) {
-        Boolean res = inst.mtSorting(state.data8, Executors.newFixedThreadPool(8));
+        Boolean res = inst.mtSorting(state.dataset, Executors.newFixedThreadPool(8));
         blackhole.consume(res);
     }
 
